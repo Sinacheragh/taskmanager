f193a486fbef5a827cc4857061f20fa519e56b82
Sinacheragh <sinacheraqi1@gmail.com>
2025-03-30 15:41:28 -0700

    sina

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console
+// arrow fuction
+let myFunction = (a, b) => a * b;
+
+//array method filter
+const ages = [32, 33, 16, 40];
+const result = ages.filter(checkAdult);
+
+function checkAdult(age) {
+  return age >= 18;
+}
+
+
+
