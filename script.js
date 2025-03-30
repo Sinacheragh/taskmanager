2729211daacee5321d2c56e9147c37bc71d26036
Sinacheragh <157360503+Sinacheragh@users.noreply.github.com>
2025-03-30 15:42:32 -0700

    Update script.js
    a

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// arrow fuction
-let myFunction = (a, b) => a * b;
 
-//array method filter
-const ages = [32, 33, 16, 40];
-const result = ages.filter(checkAdult);
-
-function checkAdult(age) {
-  return age >= 18;
-}
 
 
 
